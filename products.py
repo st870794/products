bf5a1b00119edb0703b1cbabd194a0c772bc406b
Jia Ren <st870794@gmail.com>
2020-08-13 08:44:41 +0800

    add for loop  for print product

--- products.py
+++ products.py
@@ -5,7 +5,10 @@ while True:
 		break
 	price = input('請輸入商品價格: ')
 	products.append([name, price])
-print(products)
+print(products)# 大清單
 
 # print(products[0][1])
 # 大清單第0格中的小清單第0格
+
+for p in products:# 逐一列出小清單
+	print(p[0], '的價格是', p[1])
